e2dd1a0d8001e802d82e2ef9314d1e8b0c2133dd
asutoshb <beherasutosh132@gmail.com>
2025-08-31 13:43:26 +0530

    Find duplicates in sorted array

---
+++ TwoPointers/FindDuplicatesInSortedArray.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let n = nums.length;
+    let i =0;
+    let j = 1;
+
+    while(j<n) {
+        if(nums[i] != nums[j]) {
+            i++;
+            nums[i] = nums[j];
+        }
+        j++;
+    }
+
+    return i+1;
+};
